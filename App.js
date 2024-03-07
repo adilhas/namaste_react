4d0e38e17f3c1e10afc1472295ff0de93d62bfc9
Adil Hashmi <hashmiadil161@gmail.com>
2024-03-07 12:22:14 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    "h1",
+    { id: "heading" },
+    "Hello World from React!"
+  )
+);
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World from React!"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
